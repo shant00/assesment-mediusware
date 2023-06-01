c1a615c43f6cd85fbb9c05f471e741b10f5cbbf4
shant00 <sshanto.cse@gmail.com>
2023-06-01 14:19:47 +0600

    first commit

---
+++ src/components/Problem-1.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+
+const Problem1 = () => {
+  const [show, setShow] = useState("all");
+  const [tasks, setTasks] = useState([]);
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleClick = (val) => {
+    setShow(val);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { name, status };
+    setTasks([...tasks, newTask]);
+    setName("");
+    setStatus("");
+  };
+
+  const filteredTasks = tasks
+    .filter((task) => {
+      if (show === "active") {
+        return task.status === "Active";
+      }
+      if (show === "completed") {
+        return task.status === "Completed";
+      }
+      return true; // show all tasks when 'all' is selected
+    })
+    .sort((a, b) => {
+      if (a.status === b.status) {
+        return 0; // Maintain the original order within each status category
+      }
+      if (a.status === "Active") {
+        return -1; // Active tasks show first
+      }
+      if (a.status === "Completed" && b.status !== "Active") {
+        return -1; // Completed tasks show after Active tasks
+      }
+      return 1; // Other tasks after Completed tasks
+    });
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                value={status}
+                onChange={handleStatusChange}
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" ? "active" : ""}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" ? "active" : ""}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" ? "active" : ""}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredTasks.map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Problem1;
